997b315713e51632c3e4086911e0f947d8c42661
Glad Mihai <glad@posteo.de>
2022-11-07 22:27:48 +0100

    fix: search runs by name

--- training.py
+++ training.py
@@ -112,7 +112,7 @@ def run(data_path: str, num_trials: int):
     client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
 
     runs = client.search_runs(
-        experiment_ids='2',    # Experiment ID we want
+        experiment_name='xgb-hyperopt',
         filter_string="metrics.rmse < 600",
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=1,
